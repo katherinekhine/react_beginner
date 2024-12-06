649713eb21f8d93a64f4aa19cf018eefed4c6751
katherinekhine <khaingkatherine@gmail.com>
2024-12-06 11:44:56 +0630

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-      <ul>
+      <ul className="flex gap-5 p-4 bg-red-100">
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
       </ul>
