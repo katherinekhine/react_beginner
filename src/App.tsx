a6881930201e071c6bd39f57c47586c979defe3e
katherinekhine <khaingkatherine@gmail.com>
2024-12-06 13:51:20 +0630

    update main.tsx, App.tsx, add new Route of Home.tsx, About.tsx, Projects.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
 
 function App() {
   return (
     <div>
       <ul className="flex gap-5 p-4 bg-red-100">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
+        <Link to="/" className='hover:text-red-500'>Home</Link>
+        <Link to="/about" className='hover:text-red-500'>About</Link>
+        <Link to="/projects" className='hover:text-red-500'>Projects</Link>
       </ul>
+      <main>
+       <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+       </Routes>
+      </main>
     </div>
   )
 }
