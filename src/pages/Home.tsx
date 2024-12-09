59abb547e61f954421c2da18f91c9e93cae3f224
katherinekhine <khaingkatherine@gmail.com>
2024-12-09 11:09:41 +0630

    update in Home.tsx for useState

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
 
 
 function Home() {
+  // const name:string = 'Mg Mg'
+  // hook #
+ const [name, setName] = useState<string>('Mg Mg2')
+
+ 
   return (
     <div>
-      Home
+      {/* add banner */}
+      {
+        name == "Mg Mg"?(
+          <h1 className={name === 'Mg Mg' ? 'text-red-700 text-3xl font-bold' : 'text-green-700 text-2xl font-semibold'}>Welcome {name}</h1>
+         ) : (
+          <h1 className={name === 'Mg Mg' ? 'text-red-700 text-3xl font-bold' : 'text-green-700 text-2xl font-semibold'}>Welcome Guest</h1>
+         )
+      }
+      <p className={name === 'Mg Mg' ? 'text-red-400' : 'text-green-400' }>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis cum quisquam nobis, labore officia consequatur quos error porro soluta molestiae!</p>
     </div>
   )
 }
