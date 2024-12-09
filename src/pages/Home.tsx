1b82fd3b0e11dd4f50106d7236fd444f5dc5e5da
katherinekhine <khaingkatherine@gmail.com>
2024-12-09 14:28:35 +0630

    add some function in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
 
+type ButtonProps = {
+  text: string,
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
 
 function Home() {
   // const name:string = 'Mg Mg'
@@ -9,10 +13,18 @@ function Home() {
  const handleClick = () => {
   setName(name === 'Mg Mg' ? 'Guest' : 'Mg Mg')
  }
+ const [show, setShow] = useState<boolean>(false);
+ const [count, setCount] = useState<number>(0);
  
   return (
     <div>
-      <button onClick={handleClick}>Change Text</button>
+      <h2 className={show === true ? 'block' : 'hidden'}>Counter Number is {count}</h2>
+      <Button text="Show Number Toggle" onClick={() => setShow(!show)} /> <br />
+      <Button text="Add" onClick={() => setCount(count + 1)} /> <br />
+      <Button text="Remove" onClick={() => setCount(count - 1)} /> <br />
+      <Button text="Change Text" onClick={handleClick} />
+
+      {/* <button onClick={handleClick} className="text-blue-900">Change Text</button> */}
       {/* add banner */}
       {
         name == "Mg Mg"?(
@@ -23,6 +35,12 @@ function Home() {
       }
       <p className={name === 'Mg Mg' ? 'text-red-400' : 'text-green-400' }>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis cum quisquam nobis, labore officia consequatur quos error porro soluta molestiae!</p>
     </div>
+  )
+}
+
+function Button({text, onClick}: ButtonProps) {
+  return (
+    <button onClick={onClick} className="py-2 px-4 rounded-sm bg-red-300">{text}</button>
   )
 }
 
